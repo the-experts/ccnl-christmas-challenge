63276947a87f2302df1d08443a81242c1c70b8cc
Maik Kingma <maik.kingma@codecentric.nl>
2021-11-15 16:22:35 +0100

    fixed unit tests

---
+++ src/test/java/nl/codecentric/xmascodechallenge/services/NativeChristmasImageServiceIT.java
@@ -0,0 +1,9 @@
+package nl.codecentric.xmascodechallenge.services;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+class NativeChristmasImageServiceIT extends ChristmasImageServiceTest{
+
+    // Execute the same tests but in native mode.
+}
