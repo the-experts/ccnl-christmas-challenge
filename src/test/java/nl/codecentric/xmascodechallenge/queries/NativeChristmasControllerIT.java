1f5eff46ecdb9b05dca02a36fb6bd27d4515aef1
Maik Kingma <maik.kingma@codecentric.nl>
2021-11-29 13:59:35 +0100

    fixed the tests and removed native image tests

--- src/test/java/nl/codecentric/xmascodechallenge/queries/NativeChristmasControllerIT.java
+++
@@ -1,9 +0,0 @@
-package nl.codecentric.xmascodechallenge.queries;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeChristmasControllerIT extends ChristmasControllerTest {
-
-    // Execute the same tests but in native mode.
-}
